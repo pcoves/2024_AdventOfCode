2ce46782aa646ff0cd1020d83000979db6fac3b0
jean-yves-b <jean-yves.bottraud@proton.me>
2024-12-03 22:34:35 +0100

    step2

--- rust_day2/src/main.rs
+++ rust_day2/src/main.rs
@@ -12,7 +12,8 @@ fn main() {
 							.collect::<Vec<i32>>())
 				.collect::<Vec<Vec<i32>>>();
 
-	step1(&report_list)
+	step1(&report_list);
+	step2(&report_list);
 }
 
 fn step1(list: &Vec<Vec<i32>>) {
@@ -30,4 +31,25 @@ fn is_safe(report: &Vec<i32>) -> bool {
 		let sign = (report[0] - report[1]).signum();
 		return report.windows(2).all(|w| {(w[0] - w[1]).signum() == sign && 1 <= (w[0] - w[1]).abs() && (w[0] - w[1]).abs() <= 3});
 	}
+}
+
+fn step2(list: &Vec<Vec<i32>>) {
+	let total: i32 = list.iter().map(|report| match is_safe(report){
+		true => 1,
+		false => {
+			for (index, _element) in report.iter().enumerate() {
+				if is_safe(&remove_copy(report, index)) {
+					return 1;
+				}
+			}
+			return 0;
+		},
+	}).sum();
+	println!("Step 2 Total = {}", total);
+}
+
+fn remove_copy(list: &Vec<i32>, index: usize) -> Vec<i32> {
+	let mut return_list = list.clone();
+	return_list.remove(index);
+	return return_list;
 }
