2877d4027dcfc8fa8d24252e4b500b70ffc96756
jean-yves-b <jean-yves.bottraud@proton.me>
2024-12-04 17:27:15 +0100

    step2

--- rust_day4/src/main.rs
+++ rust_day4/src/main.rs
@@ -61,3 +61,45 @@ fn step1(input: Vec<&str>) {
 		.sum::<i32>();
 	println!("step1 total : {}", total);
 }
+
+fn step2(input: Vec<&str>) {
+	let mut total:i32 = 0;
+    for row in 1..input.len()-1 {
+        for col in 1..input[0].len()-1 {
+			if 'A' == input[row].chars().nth(col).unwrap() {
+				let square = [&input[row-1][col-1..col+2], &input[row][col-1..col+2], &input[row+1][col-1..col+2]];
+				total += test_all_x_mas(&(square.join(""))) as i32;
+			}
+		}
+	}
+	println!("step2 total : {}", total);
+}
+
+fn rotate_square(square: &str) -> [char; 9] {
+	return [
+		square.chars().nth(2).unwrap(), square.chars().nth(5).unwrap(), square.chars().nth(8).unwrap(),
+		square.chars().nth(1).unwrap(), square.chars().nth(4).unwrap(), square.chars().nth(7).unwrap(),
+		square.chars().nth(0).unwrap(), square.chars().nth(3).unwrap(), square.chars().nth(6).unwrap(),];
+}
+
+fn test_x_mas(square: &str) -> bool {
+	if 'M' == square.chars().nth(0).unwrap() 
+	&& 'M' == square.chars().nth(6).unwrap() 
+	&& 'S' == square.chars().nth(2).unwrap() 
+	&& 'S' == square.chars().nth(8).unwrap(){
+		return true;
+	}
+	return false;
+}
+
+fn test_all_x_mas(square: &str) -> bool {
+	let mut rotated_square = square.to_string();
+	for _i in 0..4 {
+		if test_x_mas(&rotated_square) {
+			return true;
+		} else {
+			rotated_square = rotate_square(&rotated_square).iter().collect::<String>();
+		}
+	}
+	return false;
+}
