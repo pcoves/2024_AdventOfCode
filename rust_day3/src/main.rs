18671740d99aaec527b55b65607575941a3ada2b
jean-yves-b <jean-yves.bottraud@proton.me>
2024-12-04 01:02:31 +0100

    step2

--- rust_day3/src/main.rs
+++ rust_day3/src/main.rs
@@ -5,16 +5,17 @@ fn main() {
 	//let binding = read_to_string("files/test.txt").unwrap();
 	let binding = read_to_string("files/input.txt").unwrap();
 	
+    let re_do = Regex::new(r"(?s)don't\(\).*?(do\(\)|$)").unwrap();
+	let input = re_do.replace_all(&binding, "");
 
-	let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-	let input:i32 = re
-		.captures_iter(&binding)
-		.map(|mul| {
-				mul.get(1).unwrap().as_str().parse::<i32>().unwrap() * mul.get(2).unwrap().as_str().parse::<i32>().unwrap()
-		})
-		.collect::<Vec<i32>>()
-		.iter()
-		.sum();
+	let re_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+	let result:i32 = re_mul.captures_iter(&input)
+				.map(|mul| {
+						mul.get(1).unwrap().as_str().parse::<i32>().unwrap() * mul.get(2).unwrap().as_str().parse::<i32>().unwrap()
+				})
+				.collect::<Vec<i32>>()
+				.iter()
+				.sum();
 
-	println!("part1 total: {}", input);
+	println!("part1 total: {}", result);
 }
